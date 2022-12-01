7475bd5eb7818d1faf9883df9a62cf9b3c79512b
Tiffany Jones <eight19realty@gmail.com>
2022-12-01 09:36:58 -0500

    Added functions

--- script.js
+++ script.js
@@ -72,8 +72,27 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
+
 }
+
+var functionArray = [
+  function getUpperCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+  },
+
+  function getLowerCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+  },
+
+  function numbers() {
+    return +String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+  },
+
+  function getSpecialCharacters() {
+    return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
+  }
+
+];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
