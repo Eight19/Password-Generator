6c9c1de41c471decf1ca9a3ecf7222bfb3123c38
Tiffany Jones <eight19realty@gmail.com>
2022-12-01 23:02:00 -0500

    updated code

--- script.js
+++ script.js
@@ -1,54 +1,37 @@
-
+// Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-var randomPasswordGenerated = "";
+
 
 function generatePassword() {
-  var passwordLength = prompt(
-    "Choose between 8 and 128 characters for your password."
-  );
+  var passwordLength = passwordPrompt;
+  var passwordPrompt = prompt("Choose between 8 and 128 characters for your password.");
+   
+};
 
-}
-// var upperCases = confirm("Would you like to use uppercase letters in your password?");var lowerCases = confirm("Would you like to use lowercase letters in your password?"); var numbers = confirm("Would you like to use number in your password?");var specialCharacters = confirm("Would you like to special characters in your password?");
-
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var specialCharacters = "";
-var possible = "!#$%&()*+,-./:;<=>?@[\\]^_`{|}~";
-var ensure = "";
-
-  {
-    var upperCase = confirm(
-      "Would you like to use uppercase letters in your password?"
-    );
-    var lowerCase = confirm(
-      "Would you like to use lowercase letters in your password?"
-    );
+  if (passwordLength < 8 || passwordLength > 128) {
+     alert("Length of password must be between 8 and 128 characters")
+  } 
+  else {
+    var upperCase = confirm
+      ("Would you like to use uppercase letters in your password?");
+    var lowerCase = confirm
+      ("Would you like to use lowercase letters in your password?");
     var numbers = confirm("Would you like to use number in your password?");
-    var specialCharacters = confirm(
-      "Would you like to special characters in your password?"
-    )
-    if (passwordLength < 8 || passwordLength > 128) {
-      alert("Length of password must be between 8 and 128 characters");
-    } 
-    if (upperCase) {
-      alert[ "Has upperCase" ];
-    }
-    if (lowerCase) {
-      alert[ "Has upperCase" ];
-    }
-    if (numbers) {
-      alert[ "Has upperCase" ];
-    }
-    if (specialCharacters) {
-      alert[ "Has upperCase" ];
-    }
-  
-    var randomPasswordGenerated = "";
-var password = "";
+    var specialCharacters = confirm("Would you like to special characters in your password?");
+  }
 
-if (upperCase === true) {
+  // var upperCases = confirm("Would you like to use uppercase letters in your password?");var lowerCases = confirm("Would you like to use lowercase letters in your password?"); var numbers = confirm("Would you like to use number in your password?");var specialCharacters = confirm("Would you like to special characters in your password?");
+
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var specialCharacters = "!#$%&()*+,-./:;<=>?@[\\]^_`{|}~";
+
+  var randomPasswordGenerated = "";
+  var password = "";
+
+if (upperCase) {
   randomPasswordGenerated += upperCaseLetters;
 }
 
