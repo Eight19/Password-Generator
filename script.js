6ac46e8fe5616b9dbb9a363b1536e49a9fa919c9
Tiffany Jones <eight19realty@gmail.com>
2022-12-02 00:03:36 -0500

    update code

--- script.js
+++ script.js
@@ -6,28 +6,27 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var passwordLength = passwordPrompt;
   var passwordPrompt = prompt("Choose between 8 and 128 characters for your password.");
-   
-};
-
-  if (passwordLength < 8 || passwordLength > 128) {
-     alert("Length of password must be between 8 and 128 characters")
+}   
+  
+if (passwordLength < 8 || passwordLength > 128) {
+     alert("The length of your password must be between 8 and 128 characters.")
   } 
-  else {
-    var upperCase = confirm
-      ("Would you like to use uppercase letters in your password?");
-    var lowerCase = confirm
-      ("Would you like to use lowercase letters in your password?");
-    var numbers = confirm("Would you like to use number in your password?");
-    var specialCharacters = confirm("Would you like to special characters in your password?");
-  }
 
+  else {
+    var upperCase = confirm("Use uppercase letters in your password?");
+    var lowerCase = confirm("Use lowercase letters in your password?");
+    var numeric = confirm("Use number in your password?");
+    var specialCharacters = confirm("Use special characters in your password?");
+  }
+ 
   // var upperCases = confirm("Would you like to use uppercase letters in your password?");var lowerCases = confirm("Would you like to use lowercase letters in your password?"); var numbers = confirm("Would you like to use number in your password?");var specialCharacters = confirm("Would you like to special characters in your password?");
 
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var numbers = "0123456789";
+  var numeric = "0123456789";
   var specialCharacters = "!#$%&()*+,-./:;<=>?@[\\]^_`{|}~";
 
+ 
   var randomPasswordGenerated = "";
   var password = "";
 
@@ -39,7 +38,7 @@ if (lowerCase) {
   randomPasswordGenerated += lowerCaseLetters;
 }
 
-if (numbers) {
+if (number) {
   randomPasswordGenerated += numeric;
 }
 
@@ -47,15 +46,13 @@ if (specialCharacters) {
   randomPasswordGenerated += specialCharacters;
 }
 
-for (let i = 0; i < parseInt(passwordLength) - minimumPasswordLength; i++) {
-  var randomPasswordGenerated = Math.floor(
-    Math.random() * randomPasswordGenerated.length
-  );
+for (var i = 0; i < passwordLength; i++) {
+  var randomPasswordGenerated = Math.floor(Math.random() * randomPasswordGenerated.length);
   password += randomPasswordGenerated[random];
   console.log(random);
 }
 
-return randomPasswordGenerated;
+
 
 // Write password to the #password input
 
@@ -64,7 +61,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 }
+
+return randomPasswordGenerated;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
